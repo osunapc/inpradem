935906c28d8f503c7fc22b9e35752fb68bf1064f
osunapc <osunapc@gmail.com>
2024-10-31 13:55:14 -0400

    coorrigiendo errores de el iframe en boletin.astro

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
 	theme: {
-		colors: {
-      naranja: '#e27105',
-      azul: '#151aee',
-	  blanco:'#ffffff',
-	  negro:'#00011b'
-      // ...
-    },
-		extend: {},
+		extend: {
+			colors: {
+				naranja: "#e27105",
+				azul: "#151aee",
+				blanco: "#ffffff",
+				negro: "#00011b",
+				
+			},
+		},
 	},
 	plugins: [],
-}
+};
